d890bbac2bb56ab9f9bbfe2e493aa540181ef936
Abdulkosim Mashrapov <kasim5151@yandex.ru>
2023-04-17 18:46:13 +0600

    added petname, age

--- pets_post/models.py
+++ pets_post/models.py
@@ -14,6 +14,10 @@ class Category(models.Model):
 class Pets(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pets')
     title = models.CharField(max_length=40)
+    pet_name = models.CharField(max_length=20, default='')
+    age = models.CharField(blank=True, null=True, default='', max_length=20)
+    address = models.CharField(max_length=50, default='')
+    contact = models.CharField(max_length=40, default='')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='pets')
     breed = models.CharField(max_length=20, blank=True, null=True, default='')
     image = models.ImageField(upload_to='pets_image')
@@ -21,6 +25,5 @@ class Pets(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self) -> str:
-        return f'{self.title} - {self.owner}'
-
+        return self.title
 
